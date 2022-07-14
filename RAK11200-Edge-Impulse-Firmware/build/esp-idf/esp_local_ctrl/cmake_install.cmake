82df31ac016ff810ee226a8bf65394809a531714
Christopher Mendez <mcmchris@hotmail.com>
2022-07-14 18:20:03 -0400

    Update Build

--- RAK11200-Edge-Impulse-Firmware/build/esp-idf/esp_local_ctrl/cmake_install.cmake
+++ RAK11200-Edge-Impulse-Firmware/build/esp-idf/esp_local_ctrl/cmake_install.cmake
@@ -2,7 +2,7 @@
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/ei_firmware_esp32")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/ei_rak11200_firmware")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
